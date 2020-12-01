e6b977a8cadec43f80b81772f6a405d2cc6bff29
roikatz16 <kroi@post.bgu.ac.il>
2020-12-01 13:20:59 +0200

    #1

---
+++ frontend.py
@@ -0,0 +1,26 @@
+import mybackend
+import kivy
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
+
+
+class MyGrid(Widget):
+    query = ObjectProperty(None)
+
+    def btn(self):
+        result = mybackend.select_top_duration(1)
+        print(result)
+
+
+class MyApp(App):
+    def build(self):
+        return MyGrid()
+
+
+if __name__ == "__main__":
+    MyApp().run()
---
+++ mybackend.py
@@ -0,0 +1,30 @@
+import sqlite3
+import csv
+
+conn = sqlite3.connect('data.db')
+cur = conn.cursor()
+
+with open("../Projects/Classes/BikeShare.csv") as a_file:
+    reader = csv.reader(a_file)
+    titles = tuple(next(reader))
+    num_of_columns = len(titles)
+    question_marks = "(" + "?, "*(num_of_columns-1) + "?)"
+    cur.execute(f"CREATE TABLE if not exists bike {titles};")
+    cur.executemany(f"INSERT INTO bike VALUES {question_marks}", reader)
+    # cur.execute("SELECT * FROM data")
+    # print(cur.fetchall())
+
+
+
+def execute_query(query):
+    cur.execute(query)
+    result = cur.fetchall()
+    return result
+
+
+def select_top_duration(n):
+    return execute_query(f"Select TripDuration from bike DESC LIMIT {n}")
+
+
+ans = select_top_duration(3)
+print(ans)
