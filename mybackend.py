70288e3413b7e05b3584d51f8a0cc463cf2211c1
roikatz16 <kroi@post.bgu.ac.il>
2020-12-02 21:27:58 +0200

    #3

--- mybackend.py
+++ mybackend.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pandas as pd
+import numpy as np
 import csv
 
 
@@ -11,22 +12,23 @@ class Database:
 
     def read_database(self):
         # reader = csv.reader(a_file)
-        titles = ['StartStationName', 'EndStationName', 'UserType', 'TripDurationinmin']
-        dataset = pd.read_csv("BikeShare.csv", names=titles)
-        print(dataset.head(), dataset.shape)
+        titles = ["StartStationName", "EndStationName", "UserType", "TripDurationinmin"]
+        data_frame = pd.read_csv("BikeShare.csv", usecols=titles)
+        data_as_array = data_frame.to_numpy()
         self.cur.execute(f"CREATE TABLE if not exists bike {tuple(titles)};")
-        self.cur.executemany(f"INSERT INTO bike VALUES (?,?,?,?)", dataset)
-        print("test")
+        self.cur.executemany("INSERT INTO bike VALUES (?,?,?,?)", data_as_array)
+        self.conn.commit()
 
     def execute_query(self, query):
         self.cur.execute(query)
         result = self.cur.fetchall()
         return result
 
-    def select_top_duration(self, n):
-        return self.execute_query(f"Select TripDuration from bike DESC LIMIT {n}")
+    def select(self, n):
+        return self.execute_query(f"Select EndStationName from bike limit 5")
 
 
 my_backend = Database()
-# ans = my_backend.select_top_duration(3)
-print("Roi")
+ans = my_backend.select(3)
+for row in ans:
+    print(row)
