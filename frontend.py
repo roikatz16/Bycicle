bfa9866914a8e5e8dc8680e94768a60d4a577d39
Michal Talmor <talmormi@post.bgu.ac.il>
2020-12-07 19:56:49 +0200

    cute Shahar

--- frontend.py
+++ frontend.py
@@ -1,4 +1,6 @@
-import mybackend
+from kivy.uix.popup import Popup
+
+from mybackend import Database
 import kivy
 from kivy.app import App
 from kivy.uix.button import Button
@@ -10,6 +12,7 @@ from kivy.properties import ObjectProperty
 
 
 class MyGrid(Widget):
+    my_database = Database()
     location = ObjectProperty(None)
     time = ObjectProperty(None)
     recommendations = ObjectProperty(None)
@@ -18,10 +21,88 @@ class MyGrid(Widget):
         duration = self.time.text
         start_location = self.location.text
         num_of_result = self.recommendations.text
+        flag = True
+
+        # check that num of result is a positive number (int)
+        if not self.check_recommendions_type(num_of_result):
+            # create content and add to the popup
+            self.raise_popup("recommendations should be an integer positive number")
+            flag = False
+
+        # check that duration is a positive number (int or float)
+        if not self.check_duration_type(duration):
+            self.raise_popup("duration should be a positive number")
+            flag = False
+
+        # check if location is string
+        if not self.check_location_type(start_location):
+            self.raise_popup("location should be a string")
+            flag = False
+        else:
+            # check if location is exist
+            if not self.my_database.valid_start_location(start_location):
+                self.raise_popup("location is not exist")
+                flag = False
+
+        # if all values check pass
+        if flag:
+            result = self.get_recommendation(float(duration), start_location, int(num_of_result))
+            # make one string of end locations names
+            end_locations = ""
+            for row in result:
+                end_locations = end_locations + "\n" + row[1]
+            # pop up with names
+            self.raise_popup(end_locations)
+
+    def clear_inputs(self, text_inputs):
+        for text_input in text_inputs:
+            text_input.text = ""
+
+    def check_location_type(self, user_input):
+        try:
+            val = int(user_input)
+            return False
+        except ValueError:
+            try:
+                val = float(user_input)
+                return False
+            except ValueError:
+                # check this name in list locations by query
+                return True
+
+    def check_recommendions_type(self, user_input):
+        try:
+            val = int(user_input)
+            if val > 0:
+                return True
+            else:
+                return False
+        except ValueError:
+            return False
+
+    def check_duration_type(self, user_input):
+        try:
+            val = int(user_input)
+            if val > 0:
+                return True
+            else:
+                return False
+        except ValueError:
+            return False
+
+    def raise_popup(self, text):
+        content = Button(text=text)
+        popup = Popup(title='Error', content=content, auto_dismiss=False, size_hint=(None, None), size=(400, 400))
+
+        # bind the on_press event of the button to the dismiss function
+        content.bind(on_press=popup.dismiss)
+
+        # open the popup
+        popup.open()
 
     def get_recommendation(self, duration, start_location, num_of_result):
-        result = mybackend.select_end_stations(duration, start_location, num_of_result)
-        print(result)
+        result = self.my_database.select_end_stations(duration, start_location, num_of_result)
+        # print(result)
         return result
 
 
